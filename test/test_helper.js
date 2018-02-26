d0efac24ecce59eb9a61992e73525934c6bc387d
fibanez <fibanez@fibanez.com>
2018-02-26 14:41:13 +0000

    some comments

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,13 +9,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+// Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
+// Set up chai-jquery
 chaiJquery(chai, chai.util, $);
 
+// Build 'renderComponent' helper htat should render a given react class
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
@@ -23,14 +26,15 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
 
+// Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
+  TestUtils.Simulate[eventName](this[0]); // TestUtils.Simulate[eventName](this[0]) === TestUtils.simulate.change('<textarea />');
 };
 
 export {renderComponent, expect};
